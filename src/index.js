5dfba67ac0e109863177cf5fd7afd26b30da35c6
MarlineKhavele <marlinekhavele6@gmail.com>
2019-05-17 14:49:13 +0300

    having fun with cat API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import thunkMiddleware from "redux-thunk";
+import { createLogger } from "redux-logger";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./reducers/index";
+import * as serviceWorker from "./serviceWorker";
+const loggerMiddleware = createLogger();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleware, loggerMiddleware)
+);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/reducers/fetchCatReducer.js
@@ -0,0 +1,3 @@
+const fetchCatReducer =(state = [],action)=>{
+    
+}
